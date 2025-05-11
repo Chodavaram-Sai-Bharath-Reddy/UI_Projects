b647dc71d1686cc7ea7c11be2e2140827e097231
Chodavaram-Sai-Bharath-Reddy <Chodavaramsaibharathreddy@gmail.com>
2025-05-11 19:21:04 +0530

    initial script

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function vowelsAndConsonants(s) {
+    let v = []
+    let c = []
+    for(let i = 0; i<s.length; i++){
+        if(s[i]==='a' || s[i]==='e' || s[i]==='i' || s[i]==="o" || s[i]==='u'){
+            console.log(s[i])
+       }
+        else{
+            c.push(s[i])
+        }
+    }
+
+    for(let i=0; i<c.length; i++){
+        console.log(c[i])
+    }
+}
+
+
+function main() {
+    s = "hello world jkavascript"
+    
+    vowelsAndConsonants(s);
+}
+
+main()
+
